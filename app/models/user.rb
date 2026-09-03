7e45e982c52dc648c53ec170f79b311a2667e86c
Ubuntu <ubuntu@ip-172-31-36-205.ap-northeast-1.compute.internal>
2026-09-03 12:42:15 +0000

    [add]top page作成 ログイン認証機能とバリテーション追加

---
+++ app/models/user.rb
@@ -0,0 +1,9 @@
+class User < ApplicationRecord
+  has_secure_password
+  has_many :sessions, dependent: :destroy
+
+  validates :email_address, presence: true
+  validates :name, presence: true
+
+  normalizes :email_address, with: ->(e) { e.strip.downcase }
+end
