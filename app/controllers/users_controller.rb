d2918433c09e3693bff76e34596ae5da122b9d34
Ubuntu <ubuntu@ip-172-31-36-205.ap-northeast-1.compute.internal>
2026-09-04 11:06:53 +0000

    [add]エラーとため一旦ゲストログイン一部削除、画像投稿準備

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
 
   allow_unauthenticated_access only: [:new, :create]
-  before_action :ensure_guest_user, only: [:edit]
+  
 
   def new
     @user = User.new
@@ -10,7 +10,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      redirect_to new session_path, notice: "ユーザー登録が完了しました！"
+      redirect_to new_session_path, notice: "ユーザー登録が完了しました！"
     else
       render :new, status: :unprocessable_entity
     end
@@ -19,13 +19,7 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:name, :email_address, :password_confirmation)
+    params.require(:user).permit(:name, :email_address, :password, :password_confirmation)
   end
 
-  def ensure_guest_user
-    @user = User.find(params[:id])
-    if @user.guest_user?
-      redirect_to user_path(Current_user) , notice: "ゲストユーザーはプロフィール編集画面へ遷移できません。"
-    end
-  end  
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,16 +7,4 @@ class User < ApplicationRecord
 
   normalizes :email_address, with: ->(e) { e.strip.downcase }
   
-  GUEST_USER_EMAIL = "guest@example.com"
-
-  def self.guest
-    find_or_create_by!(email_address: GUEST_USER_EMAIL) do |user|
-      user.password = SecureRandom.urlsafe_base64
-      user.name = "guestuser"
-    end
-  end
-
-  def guest_user?
-    email_address == GUEST_USER_EMAIL
-  end
 end
