c2bd9aa0b122bbc5ac9523b95d63f1c8a68a7442
Ubuntu <ubuntu@ip-172-31-36-205.ap-northeast-1.compute.internal>
2026-09-05 11:48:14 +0000

    [add]投稿に削除機能追加

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -18,6 +18,12 @@ class PostsController < ApplicationController
     @post = Post.find(params[:id])
   end
 
+  def destroy
+    post = Post.find(params[:id])
+    post.destroy
+    redirect_to posts_path
+  end
+
   def edit
   end
 
