1bdf52e0b18f7c3247737eb793f14f35dc845c49
Ubuntu <ubuntu@ip-172-31-36-205.ap-northeast-1.compute.internal>
2026-09-06 06:41:31 +0000

    [add]投稿機能編集とlink作成

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -28,6 +28,16 @@ class PostsController < ApplicationController
   end
 
   def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    if @post.update(post_params)
+      redirect_to post_path(@post), notice: "変更を保存しました！"
+    else
+      render :edit, status: :unprocessable_entity
+    end
   end
 
   private
