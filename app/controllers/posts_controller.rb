21ccefe877722ac948f6b9d1fd5270520aaa002e
Ubuntu <ubuntu@ip-172-31-36-205.ap-northeast-1.compute.internal>
2026-09-17 02:28:43 +0000

    ポートフォリオフェーズ２スタート　検索機能追加開始

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -17,7 +17,11 @@ class PostsController < ApplicationController
   end
 
   def index
-    @posts = Post.all
+    if params[:title].present?
+      @posts = Post.where("title LIKE ?", "%#{params[:title]}%")
+    else
+      @posts = Post.all
+    end
   end
 
   def show
